9c3a0c7698c5e3513875e908a5ec5b791a6aa847
Mohammed Buirat <1192896@student.birzeit.edu>
2023-10-26 14:32:32 +0300

    Building Dto

--- MinimalAPI/MinimalAPI/DTO/AuthenticationRequestBody.cs
+++ MinimalAPI/MinimalAPI/DTO/AuthenticationRequestBody.cs
@@ -2,7 +2,7 @@
 {
     public class AuthenticationRequestBody
     {
-        string? UserName { get; set; }
-        string? Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
